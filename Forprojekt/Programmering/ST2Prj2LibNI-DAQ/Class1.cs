212dda40abd46d191a99ffb3df390e4ad0a2ff00
banasik <mbanasik@gmail.com>
2015-03-13 11:28:04 +0100

    Programmering tilføjet

---
+++ Forprojekt/Programmering/ST2Prj2LibNI-DAQ/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST2Prj2LibNI_DAQ
+{
+    public class Class1
+    {
+    }
+}
