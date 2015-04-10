45f57a6d7ec835b1451c02d7bf78ebd083f3b005
banasik <mbanasik@gmail.com>
2015-04-10 11:29:44 +0200

    opdateret med rtakker

--- Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/Logiklag.cs
+++ Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/Logiklag.cs
@@ -9,6 +9,47 @@ namespace SemesterProjekt2
     class Logiklag
     {
 
-        //test
+        //public double AntalRtakker()
+        //{
+        //    int Rtakkerdata = 0;
+        //    double tærskel = 0.8;
+
+        //    double værdi = 0;
+        //    double rTakVærdi = tærskel;
+        //    int index = 0;
+        //    int tid = 0;
+        //    bool first = true;
+
+        //    for (int i = 0; i < Dlag.GetDataListFromDaq.dataCollector.currentVoltageSeq.count; i++)
+        //    {
+
+        //        if (Dlag.GetDataListFromDaq[i] >= rTakVærdi)
+        //        {
+        //            rTakVærdi = Dlag.GetDataListFromDaq[i];
+        //            first = true;
+        //            index++;
+        //            //Rtakkerdata++;
+        //        }
+        //        else if (dataCollector.currentVoltageSeq[i] < rTakVærdi)
+        //        {
+        //            if (first == true)
+        //            {
+        //                first = false;
+        //                Rtakkerdata++;
+        //                tid = index * 4;
+        //                if (Rtakkerdata > 7)
+        //                    index = 0;
+        //            }
+        //            index++;
+        //            if (dataCollector.currentVoltageSeq[i] < tærskel)
+        //            {
+        //                rTakVærdi = tærskel;
+        //            }
+        //        }
+
+        //    }
+
+        //    return tid;
+        //    //return Rtakkerdata;
     }
 }
