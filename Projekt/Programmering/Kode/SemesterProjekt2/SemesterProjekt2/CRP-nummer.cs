91f6704bac8ddbbec3624da357c329f85d49d2f1
MalenC <m.cecilie@hotmail.com>
2015-04-10 11:22:57 +0200

    GUI lag
    Der er blevet lavet følgende GUI'er:
    Login
    CPR
    EKG
    Gammel EKG

---
+++ Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/CRP-nummer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemesterProjekt2
+{
+   public partial class CRP_nummer : Form
+   {
+      public CRP_nummer()
+      {
+         InitializeComponent();
+      }
+
+      private void label1_Click(object sender, EventArgs e)
+      {
+
+      }
+   }
+}
---
+++ Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/Gamle målinger.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SemesterProjekt2
+{
+   partial class Gamle_målinger
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+         System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+         System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+         this.textBox5 = new System.Windows.Forms.TextBox();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.textBox4 = new System.Windows.Forms.TextBox();
+         this.textBox3 = new System.Windows.Forms.TextBox();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label1 = new System.Windows.Forms.Label();
+         this.textBox2 = new System.Windows.Forms.TextBox();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+         this.label5 = new System.Windows.Forms.Label();
+         this.comboBox1 = new System.Windows.Forms.ComboBox();
+         this.label6 = new System.Windows.Forms.Label();
+         this.label7 = new System.Windows.Forms.Label();
+         this.button1 = new System.Windows.Forms.Button();
+         ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // textBox5
+         // 
+         this.textBox5.Enabled = false;
+         this.textBox5.Location = new System.Drawing.Point(28, 188);
+         this.textBox5.Name = "textBox5";
+         this.textBox5.Size = new System.Drawing.Size(147, 20);
+         this.textBox5.TabIndex = 27;
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(254, 12);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(131, 13);
+         this.label4.TabIndex = 25;
+         this.label4.Text = "CPR-nummer på patienten";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Location = new System.Drawing.Point(22, 12);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(95, 13);
+         this.label3.TabIndex = 24;
+         this.label3.Text = "Navn på patienten";
+         // 
+         // textBox4
+         // 
+         this.textBox4.Enabled = false;
+         this.textBox4.Location = new System.Drawing.Point(257, 34);
+         this.textBox4.Name = "textBox4";
+         this.textBox4.Size = new System.Drawing.Size(203, 20);
+         this.textBox4.TabIndex = 23;
+         // 
+         // textBox3
+         // 
+         this.textBox3.Enabled = false;
+         this.textBox3.Location = new System.Drawing.Point(25, 34);
+         this.textBox3.Name = "textBox3";
+         this.textBox3.Size = new System.Drawing.Size(178, 20);
+         this.textBox3.TabIndex = 22;
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(406, 331);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(30, 13);
+         this.label2.TabIndex = 19;
+         this.label2.Text = "HVR";
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(254, 331);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(27, 13);
+         this.label1.TabIndex = 18;
+         this.label1.Text = "Puls";
+         // 
+         // textBox2
+         // 
+         this.textBox2.Enabled = false;
+         this.textBox2.Location = new System.Drawing.Point(409, 358);
+         this.textBox2.Name = "textBox2";
+         this.textBox2.Size = new System.Drawing.Size(110, 20);
+         this.textBox2.TabIndex = 17;
+         // 
+         // textBox1
+         // 
+         this.textBox1.Enabled = false;
+         this.textBox1.Location = new System.Drawing.Point(257, 358);
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(112, 20);
+         this.textBox1.TabIndex = 16;
+         // 
+         // chart1
+         // 
+         chartArea4.Name = "ChartArea1";
+         this.chart1.ChartAreas.Add(chartArea4);
+         legend4.Name = "Legend1";
+         this.chart1.Legends.Add(legend4);
+         this.chart1.Location = new System.Drawing.Point(257, 72);
+         this.chart1.Name = "chart1";
+         series4.ChartArea = "ChartArea1";
+         series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+         series4.Legend = "Legend1";
+         series4.Name = "Series1";
+         this.chart1.Series.Add(series4);
+         this.chart1.Size = new System.Drawing.Size(361, 245);
+         this.chart1.TabIndex = 15;
+         this.chart1.Text = "EKG-graf";
+         // 
+         // label5
+         // 
+         this.label5.AutoSize = true;
+         this.label5.Location = new System.Drawing.Point(28, 160);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(0, 13);
+         this.label5.TabIndex = 28;
+         // 
+         // comboBox1
+         // 
+         this.comboBox1.FormattingEnabled = true;
+         this.comboBox1.Location = new System.Drawing.Point(25, 97);
+         this.comboBox1.Name = "comboBox1";
+         this.comboBox1.Size = new System.Drawing.Size(172, 21);
+         this.comboBox1.TabIndex = 29;
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.Location = new System.Drawing.Point(22, 72);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(56, 13);
+         this.label6.TabIndex = 30;
+         this.label6.Text = "Vælg dato";
+         // 
+         // label7
+         // 
+         this.label7.AutoSize = true;
+         this.label7.Location = new System.Drawing.Point(28, 160);
+         this.label7.Name = "label7";
+         this.label7.Size = new System.Drawing.Size(147, 13);
+         this.label7.TabIndex = 31;
+         this.label7.Text = "Havde patienten atrieflimmer?";
+         // 
+         // button1
+         // 
+         this.button1.Location = new System.Drawing.Point(583, 356);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(90, 21);
+         this.button1.TabIndex = 32;
+         this.button1.Text = "Gå tilbage";
+         this.button1.UseVisualStyleBackColor = true;
+         // 
+         // Gamle_målinger
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(689, 388);
+         this.Controls.Add(this.button1);
+         this.Controls.Add(this.label7);
+         this.Controls.Add(this.label6);
+         this.Controls.Add(this.comboBox1);
+         this.Controls.Add(this.label5);
+         this.Controls.Add(this.textBox5);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.textBox4);
+         this.Controls.Add(this.textBox3);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.textBox2);
+         this.Controls.Add(this.textBox1);
+         this.Controls.Add(this.chart1);
+         this.Name = "Gamle_målinger";
+         this.Text = "Gamle_målinger";
+         ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.TextBox textBox5;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.TextBox textBox4;
+      private System.Windows.Forms.TextBox textBox3;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox textBox2;
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.ComboBox comboBox1;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.Label label7;
+      private System.Windows.Forms.Button button1;
+
+   }
+}
