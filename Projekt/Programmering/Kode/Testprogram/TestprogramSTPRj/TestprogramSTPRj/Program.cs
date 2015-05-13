67063a23aabe65dd2b6148c60623afec2b59e4f9
sarakirkeby <sarasofiekirkeby@hotmail.com>
2015-05-13 10:18:47 +0200

    Testprogram

---
+++ Projekt/Programmering/Kode/Testprogram/TestprogramSTPRj/TestprogramSTPRj/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ST2Prj2LibNI_DAQ;
+
+namespace TestprogramSTPRj
+{
+    class Program
+    {
+        public static NI_DAQVoltage datacollector;
+        public static int sample;
+
+        static void Main(string[] args)
+        {
+
+            datacollector = new NI_DAQVoltage();
+            sample = datacollector.samplesPerChannel;
+            int plads = 1; 
+
+            alglib.complex[] array;                                             //bruger nyt bibliotek
+            alglib.fftr1d(datacollector.currentVoltageSeqArray, out array);     //laver vores signal om til et komplekst array. Får alle de harmoniske svingninger (Fourier trans) 
+            List<double> frekvensliste = new List<double>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double frekvens = i * (sample / array.Length / 2.0);            //Dividerer med to for at få fordoblingen af frekvenserne væk. 
+                frekvensliste.Add(frekvens);
+            }
+
+            foreach (double i in frekvensliste)
+	        {
+		    Console.WriteLine(plads + "; " + i);
+            plads++; 
+	        }
+
+            plads = 1;
+
+            foreach (double i in frekvensliste)
+            {
+                Console.WriteLine(plads + "; " + i);
+                plads++; 
+            }
+
+        }
+    }
+}
