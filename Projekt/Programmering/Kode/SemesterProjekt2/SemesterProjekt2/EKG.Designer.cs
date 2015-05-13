546d7d62702b6bbcc9c864823fb6793bb64b4d04
banasik <mbanasik@gmail.com>
2015-05-13 13:17:30 +0200

    Færdig gjort SQL til offentligdatabase - Virker!

--- Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/EKG.Designer.cs
+++ Projekt/Programmering/Kode/SemesterProjekt2/SemesterProjekt2/EKG.Designer.cs
@@ -28,185 +28,192 @@
       /// </summary>
       private void InitializeComponent()
       {
-         System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-         System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-         System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-         this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-         this.textBox1 = new System.Windows.Forms.TextBox();
-         this.textBox2 = new System.Windows.Forms.TextBox();
-         this.label1 = new System.Windows.Forms.Label();
-         this.label2 = new System.Windows.Forms.Label();
-         this.button1 = new System.Windows.Forms.Button();
-         this.textBox3 = new System.Windows.Forms.TextBox();
-         this.textBox4 = new System.Windows.Forms.TextBox();
-         this.label3 = new System.Windows.Forms.Label();
-         this.label4 = new System.Windows.Forms.Label();
-         this.button3 = new System.Windows.Forms.Button();
-         this.textBox5 = new System.Windows.Forms.TextBox();
-         this.label6 = new System.Windows.Forms.Label();
-         this.button4 = new System.Windows.Forms.Button();
-         ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-         this.SuspendLayout();
-         // 
-         // chart1
-         // 
-         chartArea1.AxisX.Title = "Tid [ms]";
-         chartArea1.Name = "ChartArea1";
-         this.chart1.ChartAreas.Add(chartArea1);
-         legend1.Name = "Legend1";
-         this.chart1.Legends.Add(legend1);
-         this.chart1.Location = new System.Drawing.Point(262, 71);
-         this.chart1.Name = "chart1";
-         series1.ChartArea = "ChartArea1";
-         series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-         series1.Legend = "Legend1";
-         series1.Name = "EKG";
-         this.chart1.Series.Add(series1);
-         this.chart1.Size = new System.Drawing.Size(361, 245);
-         this.chart1.TabIndex = 0;
-         this.chart1.Text = "EKG-graf";
-         this.chart1.Click += new System.EventHandler(this.chart1_Click);
-         // 
-         // textBox1
-         // 
-         this.textBox1.Enabled = false;
-         this.textBox1.Location = new System.Drawing.Point(645, 131);
-         this.textBox1.Name = "textBox1";
-         this.textBox1.Size = new System.Drawing.Size(112, 20);
-         this.textBox1.TabIndex = 1;
-         // 
-         // textBox2
-         // 
-         this.textBox2.Enabled = false;
-         this.textBox2.Location = new System.Drawing.Point(645, 217);
-         this.textBox2.Name = "textBox2";
-         this.textBox2.Size = new System.Drawing.Size(110, 20);
-         this.textBox2.TabIndex = 2;
-         // 
-         // label1
-         // 
-         this.label1.AutoSize = true;
-         this.label1.Location = new System.Drawing.Point(642, 104);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(27, 13);
-         this.label1.TabIndex = 3;
-         this.label1.Text = "Puls";
-         // 
-         // label2
-         // 
-         this.label2.AutoSize = true;
-         this.label2.Location = new System.Drawing.Point(642, 190);
-         this.label2.Name = "label2";
-         this.label2.Size = new System.Drawing.Size(30, 13);
-         this.label2.TabIndex = 4;
-         this.label2.Text = "HVR";
-         // 
-         // button1
-         // 
-         this.button1.Location = new System.Drawing.Point(24, 361);
-         this.button1.Name = "button1";
-         this.button1.Size = new System.Drawing.Size(113, 21);
-         this.button1.TabIndex = 5;
-         this.button1.Text = "Gem ny måling";
-         this.button1.UseVisualStyleBackColor = true;
-         this.button1.Click += new System.EventHandler(this.button1_Click_1);
-         // 
-         // textBox3
-         // 
-         this.textBox3.Enabled = false;
-         this.textBox3.Location = new System.Drawing.Point(30, 31);
-         this.textBox3.Name = "textBox3";
-         this.textBox3.Size = new System.Drawing.Size(178, 20);
-         this.textBox3.TabIndex = 7;
-         this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-         // 
-         // textBox4
-         // 
-         this.textBox4.Enabled = false;
-         this.textBox4.Location = new System.Drawing.Point(262, 31);
-         this.textBox4.Name = "textBox4";
-         this.textBox4.Size = new System.Drawing.Size(203, 20);
-         this.textBox4.TabIndex = 8;
-         this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
-         // 
-         // label3
-         // 
-         this.label3.AutoSize = true;
-         this.label3.Location = new System.Drawing.Point(30, 8);
-         this.label3.Name = "label3";
-         this.label3.Size = new System.Drawing.Size(95, 13);
-         this.label3.TabIndex = 9;
-         this.label3.Text = "Navn på patienten";
-         // 
-         // label4
-         // 
-         this.label4.AutoSize = true;
-         this.label4.Location = new System.Drawing.Point(259, 9);
-         this.label4.Name = "label4";
-         this.label4.Size = new System.Drawing.Size(131, 13);
-         this.label4.TabIndex = 10;
-         this.label4.Text = "CPR-nummer på patienten";
-         // 
-         // button3
-         // 
-         this.button3.Location = new System.Drawing.Point(29, 113);
-         this.button3.Name = "button3";
-         this.button3.Size = new System.Drawing.Size(108, 26);
-         this.button3.TabIndex = 11;
-         this.button3.Text = "Start ny måling";
-         this.button3.UseVisualStyleBackColor = true;
-         this.button3.Click += new System.EventHandler(this.button3_Click);
-         // 
-         // textBox5
-         // 
-         this.textBox5.Enabled = false;
-         this.textBox5.Location = new System.Drawing.Point(30, 183);
-         this.textBox5.Name = "textBox5";
-         this.textBox5.Size = new System.Drawing.Size(125, 20);
-         this.textBox5.TabIndex = 13;
-         // 
-         // label6
-         // 
-         this.label6.AutoSize = true;
-         this.label6.Location = new System.Drawing.Point(30, 201);
-         this.label6.Name = "label6";
-         this.label6.Size = new System.Drawing.Size(0, 13);
-         this.label6.TabIndex = 16;
-         // 
-         // button4
-         // 
-         this.button4.Location = new System.Drawing.Point(154, 359);
-         this.button4.Name = "button4";
-         this.button4.Size = new System.Drawing.Size(108, 23);
-         this.button4.TabIndex = 19;
-         this.button4.Text = "Log ud";
-         this.button4.UseVisualStyleBackColor = true;
-         this.button4.Click += new System.EventHandler(this.button4_Click);
-         // 
-         // EKG
-         // 
-         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(778, 412);
-         this.Controls.Add(this.button4);
-         this.Controls.Add(this.label6);
-         this.Controls.Add(this.textBox5);
-         this.Controls.Add(this.button3);
-         this.Controls.Add(this.label4);
-         this.Controls.Add(this.label3);
-         this.Controls.Add(this.textBox4);
-         this.Controls.Add(this.textBox3);
-         this.Controls.Add(this.button1);
-         this.Controls.Add(this.label2);
-         this.Controls.Add(this.label1);
-         this.Controls.Add(this.textBox2);
-         this.Controls.Add(this.textBox1);
-         this.Controls.Add(this.chart1);
-         this.Name = "EKG";
-         this.Text = "EKG";
-         ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-         this.ResumeLayout(false);
-         this.PerformLayout();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Title = "Time [ms]";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.Red;
+            chartArea1.AxisY.Title = "Electric potential[mV]";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Red;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(262, 71);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "EKG";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(361, 245);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "EKG-graf";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(645, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(645, 217);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(642, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Puls";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "HVR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 21);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Gem ny måling";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(30, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(262, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(203, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Navn på patienten";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "CPR-nummer på patienten";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 26);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Start ny måling";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(30, 183);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(154, 359);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Log ud";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // EKG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 436);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "EKG";
+            this.Text = "EKG";
+            this.Load += new System.EventHandler(this.EKG_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
       }
 
